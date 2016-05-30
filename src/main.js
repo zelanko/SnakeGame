4a4193f77b3e5789bb0689bc816f95950c97ed75
Ari Zelanko <zelanko@yahoo.com>
2016-05-29 20:01:55 -0400

    An arrow follows the mouse pointer.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,57 @@
-var game = new Phaser.Game(640, 480, Phaser.AUTO, 'SnakeGame-game');
-game.state.add('boot', require('./boot'));
-game.state.add('preloader', require('./preloader'));
-game.state.add('menu', require('./menu'));
-game.state.add('game', require('./game'));
-game.state.start('boot');
+var game = new Phaser.Game (
+  800,
+  600,
+  Phaser.AUTO,
+  'SnakeGame-game',
+  { create: create }
+);
+
+
+
+function create () {
+  var text = this.add.text(
+    .5,
+    .5,
+    "=========>", 
+    { font: "15px Arial",
+      fill: "#ff0044",
+      align: "center" }
+  );
+  
+  var stage = this.stage;
+  var math = this.math;
+  
+  this.input.addMoveCallback(function (pointer, x, y, isClick) {
+    var angle = math.radToDeg(math.angleBetween(0, 0, x, y));
+    text.angle = angle;
+  });
+} 
+    /*
+  game.input.maxPointers = 1;
+  game.input.mousePointer.recordRate = 1000;
+  
+  game.scale.pageAlignHorizontally = true;
+  var 
+  var create = function () {
+    text = this.add.text(
+      0,
+      0,
+      "Angle:", 
+      { font: "65px Arial",
+        fill: "#ff0044",
+        align: "center"});
+
+  text.anchor.setTo(0.5, 0.5);
+  this.input.addMoveCallback(function (pointer, x, y, isClick) {
+    var stage = pointer.game.stage;
+    var math = pointer.game.math;
+    var centerX = stage.width * .5;
+    var centerY = stage.height * .5;
+    var angle = math.radToDeg(math.angleBetween(centerX, centerY, x, y));
+    text = angle.toString();
+  });
+};
+  game.mainText = this.game.add.text(.5, .5, '====>');
+
+
+*/
